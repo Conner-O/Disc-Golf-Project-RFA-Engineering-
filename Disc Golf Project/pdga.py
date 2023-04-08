ca73c59fcdd1efca4d6c77f419ee118489e5f9e8
Conner Osborn <osborcon@iastate.edu>
2023-04-08 13:46:47 -0500

    Test Cases

--- Disc Golf Project/pdga.py
+++ Disc Golf Project/pdga.py
@@ -1,4 +1,5 @@
 import pickle
+import unittest
 
 class Player:
     def __init__(self,name,score,division,PDGAnum):
@@ -113,4 +114,18 @@ class SaveLoadData:
         return loadedRound
 
 
-    
+ # Start Test Cases
+
+class TestCourse(unittest.TestCase):
+    def testCoursePar(self):
+        course = Course("Test Course", [3, 4, 5, 4], "Test Location", 7000)
+        self.assertEqual(course.coursePar(), 16, "Course par should be 16")
+
+    def testCourseParEmptyHoles(self):
+            course = Course("Empty Course", [], "Test Location", 0)
+            self.assertEqual(course.coursePar(), 0, "Course par should be 0 for an empty course")
+
+    def testCourseParNegativeHoles(self):
+        course = Course("Negative Course", [-3, 4, -5, 4], "Test Location", 7000)
+        with self.assertRaises(ValueError):
+            course.coursePar()
