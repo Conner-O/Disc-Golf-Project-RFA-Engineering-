c8f10817731acf4c171ebe2b20be6be4b2f34985
Conner <77996006+Conner-O@users.noreply.github.com>
2023-04-11 08:44:45 -0500

    Updating syntax.

--- Disc Golf Project/test_pdga.py
+++ Disc Golf Project/test_pdga.py
@@ -1,15 +1,15 @@
 import unittest
 
 class TestCourse(unittest.TestCase):
-    def testCoursePar(self):
+    def test_CoursePar(self):
         course = Course("Test Course", [3, 4, 5, 4], "Test Location", 7000)
         self.assertEqual(course.coursePar(), 16, "Course par should be 16")
 
-    def testCourseParEmptyHoles(self):
+    def test_CourseParEmptyHoles(self):
             course = Course("Empty Course", [], "Test Location", 0)
             self.assertEqual(course.coursePar(), 0, "Course par should be 0 for an empty course")
 
-    def testCourseParNegativeHoles(self):
+    def test_CourseParNegativeHoles(self):
         course = Course("Negative Course", [-3, 4, -5, 4], "Test Location", 7000)
         with self.assertRaises(ValueError):
             course.coursePar()
