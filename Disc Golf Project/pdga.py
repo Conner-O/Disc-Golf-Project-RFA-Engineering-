7942f422031139b72bf1f876c21c039ad3b362c7
Conner <77996006+Conner-O@users.noreply.github.com>
2023-04-11 07:47:52 -0500

    Separate test cases for test case automation.

--- Disc Golf Project/pdga.py
+++ Disc Golf Project/pdga.py
@@ -1,5 +1,4 @@
 import pickle
-import unittest
 
 class Player:
     def __init__(self,name,score,division,PDGAnum):
@@ -112,20 +111,3 @@ class SaveLoadData:
             loadedRound = pickle.load(f)
         print("Round loaded!")
         return loadedRound
-
-
- # Start Test Cases
-
-class TestCourse(unittest.TestCase):
-    def testCoursePar(self):
-        course = Course("Test Course", [3, 4, 5, 4], "Test Location", 7000)
-        self.assertEqual(course.coursePar(), 16, "Course par should be 16")
-
-    def testCourseParEmptyHoles(self):
-            course = Course("Empty Course", [], "Test Location", 0)
-            self.assertEqual(course.coursePar(), 0, "Course par should be 0 for an empty course")
-
-    def testCourseParNegativeHoles(self):
-        course = Course("Negative Course", [-3, 4, -5, 4], "Test Location", 7000)
-        with self.assertRaises(ValueError):
-            course.coursePar()
