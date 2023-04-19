e8b39d7b6c9c630c983c1eeea04a2ffd11364827
Conner Osborn <osborcon@iastate.edu>
2023-04-19 16:56:23 -0500

    PEP8 Fixes

--- Disc Golf Project/test_pdga.py
+++ Disc Golf Project/test_pdga.py
@@ -1,18 +1,22 @@
 import unittest
 
+
 class TestCourse(unittest.TestCase):
     def test_CoursePar(self):
         course = Course("Test Course", [3, 4, 5, 4], "Test Location", 7000)
         self.assertEqual(course.coursePar(), 16, "Course par should be 16")
 
     def test_CourseParEmptyHoles(self):
-            course = Course("Empty Course", [], "Test Location", 0)
-            self.assertEqual(course.coursePar(), 0, "Course par should be 0 for an empty course")
+        course = Course("Empty Course", [], "Test Location", 0)
+        self.assertEqual(course.coursePar(), 0,
+                         "Course par should be 0 for an empty course")
 
     def test_CourseParNegativeHoles(self):
-        course = Course("Negative Course", [-3, 4, -5, 4], "Test Location", 7000)
+        course = Course("Negative Course",
+                        [-3, 4, -5, 4], "Test Location", 7000)
         with self.assertRaises(ValueError):
             course.coursePar()
+
 
 class TestAddPlayer(unittest.TestCase):
     def testAddPlayer(self):
@@ -20,11 +24,13 @@ class TestAddPlayer(unittest.TestCase):
         addPlayer.addPlayer()
         self.assertEqual(len(addPlayer.players), 1)
 
+
 class TestAddCourse(unittest.TestCase):
     def testAddCourse(self):
         addCourse = AddCourse()
         addCourse.addCourse()
         self.assertEqual(len(addCourse.courses), 1)
+
 
 class TestUpdateScores(unittest.TestCase):
     def testUpdateScores(self):
@@ -35,6 +41,7 @@ class TestUpdateScores(unittest.TestCase):
         updateScores.updateScores()
         self.assertEqual(player.scoreCard, [1, 2, 3])
 
+
 class TestEndRound(unittest.TestCase):
     def testEndRound(self):
         player = Player("John", 0, "MPO", 12345)
@@ -43,6 +50,7 @@ class TestEndRound(unittest.TestCase):
         endRound = EndRound(roundObj)
         endRound.endRound()
         self.assertEqual(player.score, 12)
+
 
 class TestSaveLoadData(unittest.TestCase):
     def testSaveLoadData(self):
@@ -54,5 +62,6 @@ class TestSaveLoadData(unittest.TestCase):
         loadedRoundObj = saveLoadData.loadData()
         self.assertEqual(roundObj.__dict__, loadedRoundObj.__dict__)
 
+
 if __name__ == '__main__':
-     unittest.main()
+    unittest.main()
